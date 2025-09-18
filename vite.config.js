fd5ee3d7814574a560554016fe685d6fcf7cf483
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-18 13:16:12 -0500

    more vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    port: '0.0.0.0',
     allowedHosts: "fullstackdemo-0jdr.onrender.com"
   },
 })
