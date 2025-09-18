05e31dd84d2b5c6b38c32382a20ced2651219440
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-18 11:45:43 -0500

    Inital commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic'
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <p>
+                Note app, dept of dz
+            </p>
+        </div>
+    )
+}
+
+export default Footer
