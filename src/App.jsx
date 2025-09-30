0a8a3589d5f2ab0ae7be7230b21ea76370b0ca3d
Daniel Zeegler <155176365+zeeglerPrograms@users.noreply.github.com>
2025-09-30 12:06:12 -0500

    chapter 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import Note from "./components/Note"
 import noteService from './services/notes'
 import './index.css'
+import loginService from './services/login'
 
 
 const App = () => {
@@ -10,8 +11,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -19,6 +21,15 @@ const App = () => {
       .then(initialNotes => {
         setNotes(initialNotes)
       })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
   }, [])
 
   if (!notes) {
@@ -52,12 +63,20 @@ const App = () => {
       important: Math.random() < 0.5,
     }
     
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      })
+    if (newNote.length <5 ) {
+      setErrorMessage('Note must be 5 characters or more')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    } else {
+      noteService
+        .create(noteObject)
+        .then(returnedNote => {
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
+        })
+    }
+    
     
   }
 
@@ -82,10 +101,81 @@ const App = () => {
     )
   }
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input 
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input 
+              type="password"
+              value={password}
+              onChange={({target}) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
+  )
+
+  const logOutHandler = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    window.location.reload()
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message = {errorMessage} />
+
+     {!user && loginForm()}
+     {user && (
+      <div>
+        <p>{user.name} logged in</p>
+        {noteForm()}
+        <button onClick={logOutHandler}>Log out</button>
+      </div>
+     )}
+     
+      
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important': 'all'}
@@ -96,10 +186,7 @@ const App = () => {
             <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">save</button>
-      </form>
+      
       <Footer />
     </div>
   )
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
+let token = null
+const setToken = newToken => {
+    token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     const nonExisting = {
@@ -8,12 +13,16 @@ const getAll = () => {
         content: 'This note is not saved to server',
         important: true,
     }
-    return request.then(response => response.data.concat(nonExisting))
+    return request.then(response => response.data)
 }
 
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
+const create = async newObject => {
+    const config = {
+        headers: { Authorization: token }
+    }
+
+    const response = await axios.post(baseUrl, newObject, config)
+    return response.data
 }
 
 const update = (id, newObject) => {
@@ -24,5 +33,6 @@ const update = (id, newObject) => {
 export default {
     getAll,
     create,
-    update
+    update,
+    setToken
 }
